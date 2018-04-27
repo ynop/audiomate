d99e03b77e6b066475dd603f84df7af933eec399
buec <m.buechi@outlook.com>
2018-04-27 15:42:33 +0200

    FrameOrdinalEncoder to encode label-lists with category indices (fixes #55)

--- pingu/corpus/utils/label_encoding.py
+++ pingu/corpus/utils/label_encoding.py
@@ -6,6 +6,8 @@ Then they can be converted to a numerical representation using Encoders.
 
 import numpy as np
 
+from pingu.utils import misc
+
 
 class Encoder(object):
     """
@@ -71,9 +73,9 @@ class FrameOneHotEncoder(Encoder):
         self.sr = sr
 
     def encode(self, utterance, label_list_idx='default'):
-        num_samples = utterance.num_samples(sr=self.sr)
-        num_frames = self.frame_settings.num_frames(num_samples)
         sr = self.sr or utterance.sampling_rate
+        num_samples = utterance.num_samples(sr=sr)
+        num_frames = self.frame_settings.num_frames(num_samples)
 
         mat = np.zeros((num_frames, len(self.labels)))
 
@@ -93,3 +95,69 @@ class FrameOneHotEncoder(Encoder):
                 mat[start:end, self.labels.index(label.value)] = 1
 
         return mat
+
+
+class FrameOrdinalEncoder(Encoder):
+    """
+    The FrameOrdinalEncoder is used to encode the labels per frame.
+    It creates a vector with length num-frames.
+    For every frame sets the index of the label that is present for that frame.
+    If multiple labels are present the longest within the frame.
+    If multiple labels have the same length the smaller index is selected, hence
+    the passed `labels` list acts as a priority.
+
+    Arguments:
+        labels (list): List of labels (str) which should be included in the vector representation.
+        frame_settings (FrameSettings): Frame settings to use.
+        sr (int): The sampling rate used, if None it is assumed the native sampling rate from the file is used.
+
+    Example:
+        >>> from pingu.corpus import assets
+        >>> from pingu.utils import units import
+        >>> ll = assets.LabelList(labels=[
+        >>>     assets.Label('music', 0, 2),
+        >>>     assets.Label('speech', 2, 5),
+        >>>     assets.Label('noise', 4, 6),
+        >>>     assets.Label('music', 6, 8)
+        >>> ])
+        >>> labels = ['speech', 'music', 'noise']
+        >>> fs = units.FrameSettings(16000, 16000)
+        >>> encoder = FrameOrdinalEncoder(labels, frame_settings=fs)
+        >>> encoder.encode(ll)
+        array([1,1,0,0,0,2,1,1])
+    """
+
+    def __init__(self, labels, frame_settings, sr=None):
+        self.labels = labels
+        self.frame_settings = frame_settings
+        self.sr = sr
+
+    def encode(self, utterance, label_list_idx='default'):
+        sr = self.sr or utterance.sampling_rate
+        num_samples = utterance.num_samples(sr=sr)
+        num_frames = self.frame_settings.num_frames(num_samples)
+
+        mat = np.zeros((num_frames, len(self.labels)))
+
+        if label_list_idx not in utterance.label_lists:
+            raise ValueError('Utterance {} has no label-list with idx {}'.format(utterance.idx, label_list_idx))
+
+        label_list = utterance.label_lists[label_list_idx]
+
+        for label in label_list:
+            if label.value in self.labels:
+                start, end = self.frame_settings.time_range_to_frame_range(label.start, label.end, sr)
+
+                # If label ends at the end of the utterance
+                if label.end < 0:
+                    end = num_frames
+
+                label_index = self.labels.index(label.value)
+
+                for frame_index in range(start, min(end, num_frames)):
+                    frame_start, frame_end = self.frame_settings.frame_to_seconds(frame_index, sr=sr)
+                    overlap = misc.length_of_overlap(frame_start, frame_end, label.start, label.end)
+
+                    mat[frame_index, label_index] = overlap
+
+        return np.argmax(mat, axis=1)
--- tests/corpus/utils/test_encoder.py
+++ tests/corpus/utils/test_encoder.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 
 from pingu.corpus.utils import label_encoding
+from pingu.corpus import assets
 from pingu.utils import units
 
 from tests import resources
@@ -33,5 +34,55 @@ class TestFrameOneHotEncoder(unittest.TestCase):
             [1, 0, 0],
             [1, 0, 0],
         ]).astype(np.float32)
+
+        assert np.array_equal(expected, actual)
+
+
+class TestFrameOrdinalEncoder:
+
+    def test_encode_utterance(self):
+        ds = resources.create_multi_label_corpus()
+        enc = label_encoding.FrameOrdinalEncoder(['music', 'speech', 'noise'],
+                                                 frame_settings=units.FrameSettings(32000, 16000),
+                                                 sr=16000)
+
+        actual = enc.encode(ds.utterances['utt-6'])
+        expected = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0]).astype(np.int)
+
+        assert np.array_equal(expected, actual)
+
+    def test_encode_utterance_takes_larger_label(self):
+        file = assets.File('file-idx', resources.sample_wav_file('wav_1.wav'))
+        utt = assets.Utterance('utt-idx', file, start=0, end=8)
+        ll = assets.LabelList(labels=[
+            assets.Label('music', 0, 4.5),
+            assets.Label('speech', 4.5, 8)
+        ])
+        utt.set_label_list(ll)
+
+        enc = label_encoding.FrameOrdinalEncoder(['music', 'speech', 'noise'],
+                                                 frame_settings=units.FrameSettings(32000, 16000),
+                                                 sr=16000)
+
+        actual = enc.encode(utt)
+        expected = np.array([0, 0, 0, 0, 1, 1, 1]).astype(np.int)
+
+        assert np.array_equal(expected, actual)
+
+    def test_encode_utterance_takes_lower_index_first(self):
+        file = assets.File('file-idx', resources.sample_wav_file('wav_1.wav'))
+        utt = assets.Utterance('utt-idx', file, start=0, end=5)
+        ll = assets.LabelList(labels=[
+            assets.Label('music', 0, 3),
+            assets.Label('speech', 3, 5)
+        ])
+        utt.set_label_list(ll)
+
+        enc = label_encoding.FrameOrdinalEncoder(['speech', 'music', 'noise'],
+                                                 frame_settings=units.FrameSettings(32000, 16000),
+                                                 sr=16000)
+
+        actual = enc.encode(utt)
+        expected = np.array([1, 1, 0, 0]).astype(np.int)
 
         assert np.array_equal(expected, actual)
