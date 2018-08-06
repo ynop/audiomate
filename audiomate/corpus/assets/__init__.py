1307cbc31aad24489825a68c14d4aaa850558b6a
buec <m.buechi@outlook.com>
2018-08-06 13:31:17 +0200

    Add constants for common label-list identifiers

--- audiomate/corpus/assets/__init__.py
+++ audiomate/corpus/assets/__init__.py
@@ -16,3 +16,20 @@ from .label import LabelList  # noqa: F401
 
 from .features import FeatureContainer  # noqa: F401
 from .features import PartitioningFeatureIterator  # noqa: F401
+
+# Definition of common Label-List identifiers
+LL_DOMAIN = 'domain'
+LL_DOMAIN_MUSIC = 'music'
+LL_DOMAIN_SPEECH = 'speech'
+LL_DOMAIN_NOISE = 'noise'
+
+LL_WORD_TRANSCRIPT = 'word-transcript'
+LL_WORD_TRANSCRIPT_RAW = 'word-transcript-raw'
+LL_WORD_TRANSCRIPT_ALIGNED = 'word-transcript-aligned'
+
+LL_PHONE_TRANSCRIPT = 'phone-transcript'
+LL_PHONE_TRANSCRIPT_ALIGNED = 'phone-transcript-aligned'
+
+LL_GENRE = 'genre'
+
+LL_SOUND_CLASS = 'sound-class'
