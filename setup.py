38d39acd5925e4c177a80bf75295b3d6ae25b895
buec <m.buechi@outlook.com>
2018-11-21 10:49:25 +0100

    Bump version: 2.0.0 → 3.0.0

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ except FileNotFoundError:
 ##################################################
 
 setup(name='audiomate',
-      version='2.0.0',
+      version='3.0.0',
       description=DESCRIPTION,
       long_description=long_description,
       long_description_content_type='text/markdown',
