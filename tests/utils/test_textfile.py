d470b13f863b0520a4d6e1417601ef25c778e765
buec <m.buechi@outlook.com>
2019-03-05 16:20:32 +0100

    Add option to keep empty columns when reading textfile

--- tests/utils/test_textfile.py
+++ tests/utils/test_textfile.py
@@ -32,6 +32,20 @@ def test_read_separated_lines_with_first_key():
     assert expected == records
 
 
+def test_read_separated_keeping_empty_columns():
+    file_path = os.path.join(os.path.dirname(__file__), 'empty_column_file.txt')
+
+    expected = [
+        ['a', '1', 'x', '', ''],
+        ['b', '2', '', 'y'],
+        ['c', '', '3', 'z'],
+    ]
+
+    records = textfile.read_separated_lines(file_path, separator='\t', keep_empty=True)
+
+    assert expected == records
+
+
 def test_read_key_value_lines():
     file_path = os.path.join(os.path.dirname(__file__), 'key_value_file.txt')
 
