9e8c49258c2652f1bcf42f2608be86958c185dd2
buec <m.buechi@outlook.com>
2019-03-06 19:04:33 +0100

    Update tuda-reader to load all types of files (fixes #71)

--- tests/corpus/io/test_tuda.py
+++ tests/corpus/io/test_tuda.py
@@ -1,213 +1,179 @@
 import os
 
-import pytest
-
 from audiomate import corpus
 from audiomate import issuers
 from audiomate.corpus import io
+
 from tests import resources
+from . import reader_test as rt
 
 
-@pytest.fixture
-def reader():
-    return io.TudaReader()
+class TestTudaReader(rt.CorpusReaderTest):
 
+    # SHORT aliases for speakers
+    SPK_cf = 'cf372280-5606-4b05-9d24-3ab7805d8462'
+    SPK_55 = '55065c47-1290-4974-997e-e77f24e7c72d'
+    SPK_75 = '755d9b71-f36e-45a6-a437-edebcfaee08d'
+    SPK_9e = '9e6a00c9-80f0-479d-8b36-4139a9571217'
+    SPK_58 = '58b8b441-684f-4753-aa16-589f1e149fa0'
+    SPK_2a = '2a0995a7-47d8-453f-9864-5940efd3c71a'
+    SPK_40 = '40a95aaf-2d87-43dc-b00e-9a4ceb77c6db'
 
-@pytest.fixture
-def data_path():
-    return resources.sample_corpus_path('tuda')
+    SAMPLE_PATH = resources.sample_corpus_path('tuda')
+    FILE_TRACK_BASE_PATH = SAMPLE_PATH
 
+    EXPECTED_NUMBER_OF_TRACKS = 48
+    EXPECTED_TRACKS = [
+        # DEV
+        rt.ExpFileTrack('2015-01-27-11-31-32_Kinect-Beam', 'dev/2015-01-27-11-31-32_Kinect-Beam.wav'),
+        rt.ExpFileTrack('2015-01-27-11-31-32_Kinect-RAW', 'dev/2015-01-27-11-31-32_Kinect-RAW.wav'),
+        rt.ExpFileTrack('2015-01-27-11-31-32_Realtek', 'dev/2015-01-27-11-31-32_Realtek.wav'),
+        rt.ExpFileTrack('2015-01-27-11-31-32_Samson', 'dev/2015-01-27-11-31-32_Samson.wav'),
+        rt.ExpFileTrack('2015-01-28-12-36-24_Yamaha', 'dev/2015-01-28-12-36-24_Yamaha.wav'),
+        # TEST
+        rt.ExpFileTrack('2015-01-27-12-34-36_Kinect-Beam', 'test/2015-01-27-12-34-36_Kinect-Beam.wav'),
+        rt.ExpFileTrack('2015-01-27-12-34-36_Kinect-RAW', 'test/2015-01-27-12-34-36_Kinect-RAW.wav'),
+        rt.ExpFileTrack('2015-01-27-12-34-36_Realtek', 'test/2015-01-27-12-34-36_Realtek.wav'),
+        rt.ExpFileTrack('2015-01-27-12-34-36_Samson', 'test/2015-01-27-12-34-36_Samson.wav'),
+        rt.ExpFileTrack('2015-01-27-12-34-36_Yamaha', 'test/2015-01-27-12-34-36_Yamaha.wav'),
+        # TRAIN
+        rt.ExpFileTrack('2014-03-17-10-26-07_Microsoft-Kinect-Raw',
+                        'train/2014-03-17-10-26-07_Microsoft-Kinect-Raw.wav'),
+        rt.ExpFileTrack('2014-03-17-10-26-07_Realtek', 'train/2014-03-17-10-26-07_Realtek.wav'),
+        rt.ExpFileTrack('2014-03-17-10-26-07_Yamaha', 'train/2014-03-17-10-26-07_Yamaha.wav'),
+        rt.ExpFileTrack('2014-03-17-13-03-33_Kinect-Beam', 'train/2014-03-17-13-03-33_Kinect-Beam.wav'),
+        rt.ExpFileTrack('2014-03-17-13-03-33_Realtek', 'train/2014-03-17-13-03-33_Realtek.wav'),
+        rt.ExpFileTrack('2014-03-17-13-03-33_Yamaha', 'train/2014-03-17-13-03-33_Yamaha.wav'),
+        rt.ExpFileTrack('2014-03-19-15-01-56_Kinect-Beam', 'train/2014-03-19-15-01-56_Kinect-Beam.wav'),
+        # NOT ALL LISTED
+    ]
 
-class TestTudaReader:
+    EXPECTED_NUMBER_OF_ISSUERS = 7
+    EXPECTED_ISSUERS = [
+        rt.ExpSpeaker(SPK_cf, 8, issuers.Gender.MALE, issuers.AgeGroup.ADULT, 'deu'),
+        rt.ExpSpeaker(SPK_55, 10, issuers.Gender.MALE, issuers.AgeGroup.ADULT, 'deu'),
+        rt.ExpSpeaker(SPK_75, 7, issuers.Gender.MALE, issuers.AgeGroup.ADULT, 'deu'),
+        rt.ExpSpeaker(SPK_9e, 5, issuers.Gender.FEMALE, issuers.AgeGroup.YOUTH, 'deu'),
+        rt.ExpSpeaker(SPK_58, 10, issuers.Gender.FEMALE, issuers.AgeGroup.ADULT, 'deu'),
+        rt.ExpSpeaker(SPK_2a, 5, issuers.Gender.MALE, issuers.AgeGroup.ADULT, 'deu'),
+        rt.ExpSpeaker(SPK_40, 3, issuers.Gender.MALE, issuers.AgeGroup.ADULT, 'deu'),
+    ]
 
-    def test_get_ids_from_folder(self, data_path):
-        assert io.TudaReader.get_ids_from_folder(os.path.join(data_path, 'train'), 'train') == {
+    EXPECTED_NUMBER_OF_UTTERANCES = 48
+    EXPECTED_UTTERANCES = [
+        # DEV
+        rt.ExpUtterance('2015-01-27-11-31-32_Kinect-Beam', '2015-01-27-11-31-32_Kinect-Beam', SPK_75, 0, -1),
+        rt.ExpUtterance('2015-01-27-11-31-32_Kinect-RAW', '2015-01-27-11-31-32_Kinect-RAW', SPK_75, 0, -1),
+        rt.ExpUtterance('2015-01-27-11-31-32_Realtek', '2015-01-27-11-31-32_Realtek', SPK_75, 0, -1),
+        rt.ExpUtterance('2015-01-27-11-31-32_Samson', '2015-01-27-11-31-32_Samson', SPK_75, 0, -1),
+        rt.ExpUtterance('2015-01-28-12-36-24_Yamaha', '2015-01-28-12-36-24_Yamaha', SPK_9e, 0, -1),
+        # TEST
+        rt.ExpUtterance('2015-01-27-12-34-36_Kinect-Beam', '2015-01-27-12-34-36_Kinect-Beam', SPK_58, 0, -1),
+        rt.ExpUtterance('2015-01-27-12-34-36_Kinect-RAW', '2015-01-27-12-34-36_Kinect-RAW', SPK_58, 0, -1),
+        rt.ExpUtterance('2015-01-27-12-34-36_Realtek', '2015-01-27-12-34-36_Realtek', SPK_58, 0, -1),
+        rt.ExpUtterance('2015-01-27-12-34-36_Samson', '2015-01-27-12-34-36_Samson', SPK_58, 0, -1),
+        rt.ExpUtterance('2015-01-27-12-34-36_Yamaha', '2015-01-27-12-34-36_Yamaha', SPK_58, 0, -1),
+        # TRAIN
+        rt.ExpUtterance('2014-03-17-10-26-07_Microsoft-Kinect-Raw',
+                        '2014-03-17-10-26-07_Microsoft-Kinect-Raw', SPK_40, 0, -1),
+        rt.ExpUtterance('2014-03-17-10-26-07_Realtek', '2014-03-17-10-26-07_Realtek', SPK_40, 0, -1),
+        rt.ExpUtterance('2014-03-17-10-26-07_Yamaha', '2014-03-17-10-26-07_Yamaha', SPK_40, 0, -1),
+        rt.ExpUtterance('2014-03-17-13-03-33_Kinect-Beam', '2014-03-17-13-03-33_Kinect-Beam', SPK_cf, 0, -1),
+        rt.ExpUtterance('2014-03-17-13-03-33_Realtek', '2014-03-17-13-03-33_Realtek', SPK_cf, 0, -1),
+        rt.ExpUtterance('2014-03-17-13-03-33_Yamaha', '2014-03-17-13-03-33_Yamaha', SPK_cf, 0, -1),
+        rt.ExpUtterance('2014-03-19-15-01-56_Kinect-Beam', '2014-03-19-15-01-56_Kinect-Beam', SPK_cf, 0, -1),
+        # NOT ALL LISTED
+    ]
+
+    EXPECTED_LABEL_LISTS = {
+        '2015-01-27-11-31-32_Samson': [
+            rt.ExpLabelList(corpus.LL_WORD_TRANSCRIPT, 1),
+            rt.ExpLabelList(corpus.LL_WORD_TRANSCRIPT_RAW, 1),
+        ],
+        '2015-01-27-11-31-32_Realtek': [
+            rt.ExpLabelList(corpus.LL_WORD_TRANSCRIPT, 1),
+            rt.ExpLabelList(corpus.LL_WORD_TRANSCRIPT_RAW, 1),
+        ],
+        '2014-03-17-10-26-07_Realtek': [
+            rt.ExpLabelList(corpus.LL_WORD_TRANSCRIPT, 1),
+            rt.ExpLabelList(corpus.LL_WORD_TRANSCRIPT_RAW, 1),
+        ],
+        # NOT ALL LISTED
+    }
+
+    EXPECTED_LABELS = {
+        '2015-01-27-11-31-32_Samson': [
+            rt.ExpLabel(corpus.LL_WORD_TRANSCRIPT, 0, 'Manche haben dass', 0, -1),
+            rt.ExpLabel(corpus.LL_WORD_TRANSCRIPT_RAW, 0, 'Manche haben , dass.', 0, -1),
+        ],
+        '2015-01-27-11-31-32_Realtek': [
+            rt.ExpLabel(corpus.LL_WORD_TRANSCRIPT, 0, 'Manche haben dass', 0, -1),
+            rt.ExpLabel(corpus.LL_WORD_TRANSCRIPT_RAW, 0, 'Manche haben , dass.', 0, -1),
+        ],
+        '2014-03-17-13-03-33_Realtek': [
+            rt.ExpLabel(corpus.LL_WORD_TRANSCRIPT, 0, 'Ich habe mich', 0, -1),
+            rt.ExpLabel(corpus.LL_WORD_TRANSCRIPT_RAW, 0, 'Ich habe mich.', 0, -1),
+        ],
+        # NOT ALL LISTED
+    }
+
+    EXPECTED_NUMBER_OF_SUBVIEWS = 3 + 6 + 5 + 5 + 6
+    EXPECTED_SUBVIEWS = [
+        rt.ExpSubview('test', [
+            '2015-01-27-12-34-36_Kinect-Beam',
+            '2015-01-27-12-34-36_Kinect-RAW',
+            '2015-01-27-12-34-36_Realtek',
+            '2015-01-27-12-34-36_Samson',
+            '2015-01-27-12-34-36_Yamaha',
+            '2015-02-03-12-08-13_Kinect-Beam',
+            '2015-02-03-12-08-13_Kinect-RAW',
+            '2015-02-03-12-08-13_Realtek',
+            '2015-02-03-12-08-13_Samson',
+            '2015-02-03-12-08-13_Yamaha',
+            '2015-02-10-14-31-52_Kinect-Beam',
+            '2015-02-10-14-31-52_Kinect-RAW',
+            '2015-02-10-14-31-52_Realtek',
+            '2015-02-10-14-31-52_Samson',
+            '2015-02-10-14-31-52_Yamaha',
+        ]),
+        rt.ExpSubview('Microsoft-Kinect-Raw', [
+            '2014-03-17-10-26-07_Microsoft-Kinect-Raw',
+            '2014-03-17-13-03-33_Microsoft-Kinect-Raw',
+        ]),
+        rt.ExpSubview('train_Samson', [
+            '2014-08-07-13-22-38_Samson',
+            '2014-08-14-14-52-00_Samson',
+        ]),
+        # NOT ALL LISTED
+    ]
+
+    def load(self):
+        reader = io.TudaReader()
+        return reader.load(self.SAMPLE_PATH)
+
+    def test_get_ids_from_folder(self):
+        assert io.TudaReader.get_ids_from_folder(os.path.join(self.SAMPLE_PATH, 'train'), 'train') == {
+            '2014-03-17-10-26-07',
             '2014-03-17-13-03-33',
             '2014-03-19-15-01-56',
             '2014-08-07-13-22-38',
-            '2014-08-14-14-52-00'
+            '2014-08-14-14-52-00',
+            '2015-04-17-11-26-07',
         }
-        assert io.TudaReader.get_ids_from_folder(os.path.join(data_path, 'dev'), 'dev') == {
+        assert io.TudaReader.get_ids_from_folder(os.path.join(self.SAMPLE_PATH, 'dev'), 'dev') == {
             '2015-01-27-11-31-32',
             '2015-01-28-11-35-47',
-            '2015-01-28-12-36-24'
+            '2015-01-28-12-36-24',
         }
-        assert io.TudaReader.get_ids_from_folder(os.path.join(data_path, 'test'), 'test') == {
+        assert io.TudaReader.get_ids_from_folder(os.path.join(self.SAMPLE_PATH, 'test'), 'test') == {
             '2015-01-27-12-34-36',
             '2015-02-03-12-08-13',
-            '2015-02-10-14-31-52'
+            '2015-02-10-14-31-52',
         }
 
-    def test_get_ids_from_folder_ignore_bad_files(self, data_path):
-        ids = io.TudaReader.get_ids_from_folder(os.path.join(data_path, 'train'), 'train')
+    def test_get_ids_from_folder_ignore_bad_files(self):
+        ids = io.TudaReader.get_ids_from_folder(os.path.join(self.SAMPLE_PATH, 'train'), 'train')
         assert '2014-08-05-11-08-34' not in ids
 
-        ids = io.TudaReader.get_ids_from_folder(os.path.join(data_path, 'dev'), 'dev')
+        ids = io.TudaReader.get_ids_from_folder(os.path.join(self.SAMPLE_PATH, 'dev'), 'dev')
         assert '2015-01-28-11-49-53' not in ids
-
-    def test_load_tracks(self, reader, data_path):
-        ds = reader.load(data_path)
-
-        dev = os.path.join(data_path, 'dev')
-        test = os.path.join(data_path, 'test')
-        train = os.path.join(data_path, 'train')
-
-        assert ds.num_tracks == 10
-
-        assert ds.tracks['2014-03-17-13-03-33'].idx == '2014-03-17-13-03-33'
-        assert ds.tracks['2014-03-17-13-03-33'].path == os.path.join(train, '2014-03-17-13-03-33_Kinect-Beam.wav')
-
-        assert ds.tracks['2014-03-19-15-01-56'].idx == '2014-03-19-15-01-56'
-        assert ds.tracks['2014-03-19-15-01-56'].path == os.path.join(train, '2014-03-19-15-01-56_Kinect-Beam.wav')
-
-        assert ds.tracks['2014-08-07-13-22-38'].idx == '2014-08-07-13-22-38'
-        assert ds.tracks['2014-08-07-13-22-38'].path == os.path.join(train, '2014-08-07-13-22-38_Kinect-Beam.wav')
-
-        assert ds.tracks['2014-08-14-14-52-00'].idx == '2014-08-14-14-52-00'
-        assert ds.tracks['2014-08-14-14-52-00'].path == os.path.join(train, '2014-08-14-14-52-00_Kinect-Beam.wav')
-
-        assert ds.tracks['2015-01-27-11-31-32'].idx == '2015-01-27-11-31-32'
-        assert ds.tracks['2015-01-27-11-31-32'].path == os.path.join(dev, '2015-01-27-11-31-32_Kinect-Beam.wav')
-
-        assert ds.tracks['2015-01-28-11-35-47'].idx == '2015-01-28-11-35-47'
-        assert ds.tracks['2015-01-28-11-35-47'].path == os.path.join(dev, '2015-01-28-11-35-47_Kinect-Beam.wav')
-
-        assert ds.tracks['2015-01-28-12-36-24'].idx == '2015-01-28-12-36-24'
-        assert ds.tracks['2015-01-28-12-36-24'].path == os.path.join(dev, '2015-01-28-12-36-24_Kinect-Beam.wav')
-
-        assert ds.tracks['2015-01-27-12-34-36'].idx == '2015-01-27-12-34-36'
-        assert ds.tracks['2015-01-27-12-34-36'].path == os.path.join(test, '2015-01-27-12-34-36_Kinect-Beam.wav')
-
-        assert ds.tracks['2015-02-03-12-08-13'].idx == '2015-02-03-12-08-13'
-        assert ds.tracks['2015-02-03-12-08-13'].path == os.path.join(test, '2015-02-03-12-08-13_Kinect-Beam.wav')
-
-        assert ds.tracks['2015-02-10-14-31-52'].idx == '2015-02-10-14-31-52'
-        assert ds.tracks['2015-02-10-14-31-52'].path == os.path.join(test, '2015-02-10-14-31-52_Kinect-Beam.wav')
-
-    def test_load_speakers(self, reader, data_path):
-        ds = reader.load(data_path)
-
-        assert ds.num_issuers == 6
-
-        assert ds.issuers['cf372280-5606-4b05-9d24-3ab7805d8462'].idx == 'cf372280-5606-4b05-9d24-3ab7805d8462'
-        assert type(ds.issuers['cf372280-5606-4b05-9d24-3ab7805d8462']) == issuers.Speaker
-        assert ds.issuers['cf372280-5606-4b05-9d24-3ab7805d8462'].gender == issuers.Gender.MALE
-        assert ds.issuers['cf372280-5606-4b05-9d24-3ab7805d8462'].age_group == issuers.AgeGroup.ADULT
-        assert ds.issuers['cf372280-5606-4b05-9d24-3ab7805d8462'].native_language == 'deu'
-
-        assert ds.issuers['55065c47-1290-4974-997e-e77f24e7c72d'].idx == '55065c47-1290-4974-997e-e77f24e7c72d'
-        assert type(ds.issuers['55065c47-1290-4974-997e-e77f24e7c72d']) == issuers.Speaker
-        assert ds.issuers['55065c47-1290-4974-997e-e77f24e7c72d'].gender == issuers.Gender.MALE
-        assert ds.issuers['55065c47-1290-4974-997e-e77f24e7c72d'].age_group == issuers.AgeGroup.ADULT
-        assert ds.issuers['55065c47-1290-4974-997e-e77f24e7c72d'].native_language == 'deu'
-
-        assert ds.issuers['755d9b71-f36e-45a6-a437-edebcfaee08d'].idx == '755d9b71-f36e-45a6-a437-edebcfaee08d'
-        assert type(ds.issuers['755d9b71-f36e-45a6-a437-edebcfaee08d']) == issuers.Speaker
-        assert ds.issuers['755d9b71-f36e-45a6-a437-edebcfaee08d'].gender == issuers.Gender.MALE
-        assert ds.issuers['755d9b71-f36e-45a6-a437-edebcfaee08d'].age_group == issuers.AgeGroup.ADULT
-        assert ds.issuers['755d9b71-f36e-45a6-a437-edebcfaee08d'].native_language == 'deu'
-
-        assert ds.issuers['9e6a00c9-80f0-479d-8b36-4139a9571217'].idx == '9e6a00c9-80f0-479d-8b36-4139a9571217'
-        assert type(ds.issuers['9e6a00c9-80f0-479d-8b36-4139a9571217']) == issuers.Speaker
-        assert ds.issuers['9e6a00c9-80f0-479d-8b36-4139a9571217'].gender == issuers.Gender.FEMALE
-        assert ds.issuers['9e6a00c9-80f0-479d-8b36-4139a9571217'].age_group == issuers.AgeGroup.YOUTH
-        assert ds.issuers['9e6a00c9-80f0-479d-8b36-4139a9571217'].native_language == 'deu'
-
-        assert ds.issuers['58b8b441-684f-4753-aa16-589f1e149fa0'].idx == '58b8b441-684f-4753-aa16-589f1e149fa0'
-        assert type(ds.issuers['58b8b441-684f-4753-aa16-589f1e149fa0']) == issuers.Speaker
-        assert ds.issuers['58b8b441-684f-4753-aa16-589f1e149fa0'].gender == issuers.Gender.FEMALE
-        assert ds.issuers['58b8b441-684f-4753-aa16-589f1e149fa0'].age_group == issuers.AgeGroup.ADULT
-        assert ds.issuers['58b8b441-684f-4753-aa16-589f1e149fa0'].native_language == 'deu'
-
-        assert ds.issuers['2a0995a7-47d8-453f-9864-5940efd3c71a'].idx == '2a0995a7-47d8-453f-9864-5940efd3c71a'
-        assert type(ds.issuers['2a0995a7-47d8-453f-9864-5940efd3c71a']) == issuers.Speaker
-        assert ds.issuers['2a0995a7-47d8-453f-9864-5940efd3c71a'].gender == issuers.Gender.MALE
-        assert ds.issuers['2a0995a7-47d8-453f-9864-5940efd3c71a'].age_group == issuers.AgeGroup.ADULT
-        assert ds.issuers['2a0995a7-47d8-453f-9864-5940efd3c71a'].native_language == 'deu'
-
-    def test_load_utterances(self, reader, data_path):
-        ds = reader.load(data_path)
-
-        assert ds.num_utterances == 10
-
-        assert ds.utterances['2014-03-17-13-03-33'].idx == '2014-03-17-13-03-33'
-        assert ds.utterances['2014-03-17-13-03-33'].track.idx == '2014-03-17-13-03-33'
-
-        assert ds.utterances['2014-03-19-15-01-56'].idx == '2014-03-19-15-01-56'
-        assert ds.utterances['2014-03-19-15-01-56'].track.idx == '2014-03-19-15-01-56'
-
-        assert ds.utterances['2014-08-07-13-22-38'].idx == '2014-08-07-13-22-38'
-        assert ds.utterances['2014-08-07-13-22-38'].track.idx == '2014-08-07-13-22-38'
-
-        assert ds.utterances['2014-08-14-14-52-00'].idx == '2014-08-14-14-52-00'
-        assert ds.utterances['2014-08-14-14-52-00'].track.idx == '2014-08-14-14-52-00'
-
-        assert ds.utterances['2015-01-27-11-31-32'].idx == '2015-01-27-11-31-32'
-        assert ds.utterances['2015-01-27-11-31-32'].track.idx == '2015-01-27-11-31-32'
-
-        assert ds.utterances['2015-01-28-11-35-47'].idx == '2015-01-28-11-35-47'
-        assert ds.utterances['2015-01-28-11-35-47'].track.idx == '2015-01-28-11-35-47'
-
-        assert ds.utterances['2015-01-28-12-36-24'].idx == '2015-01-28-12-36-24'
-        assert ds.utterances['2015-01-28-12-36-24'].track.idx == '2015-01-28-12-36-24'
-
-        assert ds.utterances['2015-01-27-12-34-36'].idx == '2015-01-27-12-34-36'
-        assert ds.utterances['2015-01-27-12-34-36'].track.idx == '2015-01-27-12-34-36'
-
-        assert ds.utterances['2015-02-03-12-08-13'].idx == '2015-02-03-12-08-13'
-        assert ds.utterances['2015-02-03-12-08-13'].track.idx == '2015-02-03-12-08-13'
-
-        assert ds.utterances['2015-02-10-14-31-52'].idx == '2015-02-10-14-31-52'
-        assert ds.utterances['2015-02-10-14-31-52'].track.idx == '2015-02-10-14-31-52'
-
-    def test_load_transcriptions(self, reader, data_path):
-        ds = reader.load(data_path)
-
-        utt = ds.utterances['2014-03-17-13-03-33']
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT][0].value == 'Ich habe mich'
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT_RAW][0].value == 'Ich habe mich.'
-
-        utt = ds.utterances['2014-03-19-15-01-56']
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT][0].value == 'Felsnester hoch'
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT_RAW][0].value == 'Felsnester hoch.'
-
-        utt = ds.utterances['2014-08-07-13-22-38']
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT][0].value == 'Ja'
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT_RAW][0].value == 'Ja.'
-
-        utt = ds.utterances['2014-08-14-14-52-00']
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT][0].value == 'Ihr graubraunes'
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT_RAW][0].value == 'Ihr graubraunes.'
-
-        utt = ds.utterances['2015-01-27-11-31-32']
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT][0].value == 'Manche haben dass'
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT_RAW][0].value == 'Manche haben , dass.'
-
-        utt = ds.utterances['2015-01-28-11-35-47']
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT][0].value == 'Juni neun'
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT_RAW][0].value == 'Juni 1919.'
-
-        utt = ds.utterances['2015-01-28-12-36-24']
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT][0].value == 'enthalten'
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT_RAW][0].value == 'enthalten.'
-
-        utt = ds.utterances['2015-01-27-12-34-36']
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT][0].value == 'Unabhängigkeit'
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT_RAW][0].value == 'Unabhängigkeit .'
-
-        utt = ds.utterances['2015-02-03-12-08-13']
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT][0].value == 'Was los ist'
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT_RAW][0].value == 'Was los ist?'
-
-        utt = ds.utterances['2015-02-10-14-31-52']
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT][0].value == 'Jetzt kennenlernen'
-        assert utt.label_lists[corpus.LL_WORD_TRANSCRIPT_RAW][0].value == 'Jetzt kennenlernen.'
-
-    def test_load_subviews(self, reader, data_path):
-        ds = reader.load(data_path)
-
-        assert ds.num_subviews == 3
-
-        assert ds.subviews['train'].num_utterances == 4
-        assert ds.subviews['dev'].num_utterances == 3
-        assert ds.subviews['test'].num_utterances == 3
