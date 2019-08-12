678b6be0d47265580580b92af91153a112ef0b60
buec <m.buechi@outlook.com>
2019-08-12 13:04:15 +0200

    Exclude invalidated subset in common voice reader

--- audiomate/corpus/io/common_voice.py
+++ audiomate/corpus/io/common_voice.py
@@ -9,6 +9,34 @@ from audiomate.utils import textfile
 from . import base
 
 
+INVALID_UTTS = [
+    'common_voice_de_17876483',
+    'common_voice_de_17876495',
+    'common_voice_de_17876535',
+    'common_voice_de_17876537',
+    'common_voice_de_17876539',
+    'common_voice_de_17876540',
+    'common_voice_de_17876541',
+    'common_voice_de_17876542',
+    'common_voice_de_17876543',
+    'common_voice_de_17876544',
+    'common_voice_de_17876545',
+    'common_voice_de_17876546',
+    'common_voice_de_17876547',
+    'common_voice_de_17876548',
+    'common_voice_de_17876549',
+    'common_voice_de_17876550',
+    'common_voice_de_17876551',
+    'common_voice_de_17876552',
+    'common_voice_de_17876553',
+    'common_voice_de_17876554',
+    'common_voice_de_17876555',
+    'common_voice_de_17876556',
+    'common_voice_de_17876557',
+    'common_voice_de_17876304'
+]
+
+
 class CommonVoiceReader(base.CorpusReader):
     """
     Reader for the Common Voice Corpus.
@@ -43,7 +71,11 @@ class CommonVoiceReader(base.CorpusReader):
         for path in glob.glob(os.path.join(path, '*.tsv')):
             file_name = os.path.split(path)[1]
             basename = os.path.splitext(file_name)[0]
-            all.append(basename)
+
+            # We don't want to include the invalidated files
+            # since there maybe corrupt files
+            if basename != 'invalidated':
+                all.append(basename)
 
         return all
 
@@ -62,13 +94,14 @@ class CommonVoiceReader(base.CorpusReader):
         )
 
         for entry in entries:
-
             file_idx = CommonVoiceReader.create_assets_if_needed(
                 corpus,
                 path,
                 entry
             )
-            subset_utt_ids.append(file_idx)
+
+            if file_idx is not None:
+                subset_utt_ids.append(file_idx)
 
         filter = subset.MatchingUtteranceIdxFilter(utterance_idxs=set(subset_utt_ids))
         subview = subset.Subview(corpus, filter_criteria=[filter])
@@ -77,17 +110,27 @@ class CommonVoiceReader(base.CorpusReader):
     @staticmethod
     def create_assets_if_needed(corpus, path, entry):
         """ Create File/Utterance/Issuer, if they not already exist and return utt-idx. """
-        file_idx = entry[1]
+        file_name = entry[1]
+        file_idx, _ = os.path.splitext(file_name)
+
+        if file_idx in INVALID_UTTS:
+            return None
 
         if file_idx not in corpus.utterances.keys():
             speaker_idx = entry[0]
             transcription = entry[2]
 
-            age = CommonVoiceReader.map_age(entry[5])
-            gender = CommonVoiceReader.map_gender(entry[6])
+            if len(entry) >= 6:
+                age = CommonVoiceReader.map_age(entry[5])
+            else:
+                age = issuers.AgeGroup.UNKNOWN
 
-            file_path = os.path.join(path, 'clips', '{}.mp3'.format(file_idx))
+            if len(entry) >= 7:
+                gender = CommonVoiceReader.map_gender(entry[6])
+            else:
+                gender = issuers.Gender.UNKNOWN
 
+            file_path = os.path.join(path, 'clips', file_name)
             corpus.new_file(file_path, file_idx)
 
             if speaker_idx in corpus.issuers.keys():
